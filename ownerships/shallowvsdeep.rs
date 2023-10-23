a1e0612e4f88944e847ea4741b863bc071361505
rupam-04 <rpmdey2004@gmail.com>
2023-10-23 21:00:44 +0530

    .

--- ownerships/shallowvsdeep.rs
+++ ownerships/shallowvsdeep.rs
@@ -3,7 +3,7 @@ fn main()
     let s1 = String::from("hello");
     let s2 = s1;
 
-    //println!("{}, world!", s1);
+    //println!("{}, world!", s1); if it was an integer instead of string it would have worked
     let s1 = String::from("hello");
     let s2 = s1.clone();
 
